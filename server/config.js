77ee9279173d1ec97d1dc16e67a12bc7aa5cf5a2
张榜 <bang.zhang@liulishuo.com>
2018-06-04 21:02:02 +0800

    基本配置+数据库+一个demo后端api

--- server/config.js
+++ server/config.js
@@ -1,4 +1,15 @@
 const CONF = {
+    // 其他配置 ...
+    serverHost: 'localhost',
+    tunnelServerUrl: '',
+    tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
+      // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.cloud.tencent.com/capi
+    qcloudAppId: '1256865217',
+    qcloudSecretId: 'AKID83j9QSjoFSP8czq0iAMIxMVR7YljJt52',
+    qcloudSecretKey: 'qQkna8QSFm35rNefQbFfpkPnUvAC1d6M',
+    wxMessageToken: 'weixinmsgtoken',
+    networkTimeout: 30000,
+
     port: '5757',
     rootPathname: '',
 
@@ -21,7 +32,7 @@ const CONF = {
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        pass: '',
+        pass: '!zb930306',
         char: 'utf8mb4'
     },
 
